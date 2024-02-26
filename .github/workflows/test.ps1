ace7bbb52e784df0803bb4e98dbf9afdfcd73cba
Florin Strugariu <florin.strugariu@uipath.com>
2024-02-26 22:51:00 +0200

    as

--- .github/workflows/test.ps1
+++ .github/workflows/test.ps1
@@ -26,9 +26,9 @@ The filter to be applied on TestSet Names during the GET request to the TestSets
 
 # Declare parameters
 param(
-    [string]$client_id = ${ secrets.CLIENT_ID },
-    [string]$client_secret = ${ secrets.CLIENT_SECRET },
-    [string]$scope = ${ secrets.SCOPE },
+    [string]$client_id = ${ env.CLIENT_ID },
+    [string]$client_secret = ${ env.CLIENT_SECRET },
+    [string]$scope = ${ env.SCOPE },
     [string]$org = ${ env.ACCOUNTFORAPP },
     [string]$tenant = ${ env.OR_TENANT },
     [string]$filter = "RegTest"
